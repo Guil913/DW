5a67a643545ecb7b464f10b6ba73e8710178d7a1
Guil913 <guilhermecoelho913@gmail.com>
2024-12-14 03:54:11 +0000

    x button works

--- scripts/general.js
+++ scripts/general.js
@@ -1,24 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const sidenav = document.querySelector(".sidenav");
-    const sidenavToggle = document.querySelector(".sidenav-toggle");
     const blackSquare = document.querySelector(".black-square");
     const buttons = document.querySelectorAll(".button");
 
     let currentInput = "";
     let isDatePart1Complete = false;
-
-    sidenavToggle.addEventListener("click", function() {
-        sidenav.classList.toggle("collapsed");
-
-        const header = document.querySelector("header");
-        if (sidenav.classList.contains("collapsed")) {
-            header.style.marginLeft = "0";  
-            header.style.width = "100%";  
-        } else {
-            header.style.marginLeft = "20%";  
-            header.style.width = "calc(100% - 20%)";  
-        }
-    });
 
     buttons.forEach(button => {
         button.addEventListener("click", function() {
@@ -39,4 +24,10 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     });
+
+    window.clearDisplay = function() {
+        currentInput = "";
+        isDatePart1Complete = false;
+        blackSquare.textContent = '';
+    };
 });
