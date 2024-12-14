872afa6af871242ecea27c91e7fd28d6dfb6d741
Guil913 <guilhermecoelho913@gmail.com>
2024-12-14 04:05:07 +0000

    falta o / erro

--- scripts/general.js
+++ scripts/general.js
@@ -1,9 +1,26 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const sidenavToggle = document.querySelector(".sidenav-toggle");
+    const sidenav = document.querySelector(".sidenav");
     const blackSquare = document.querySelector(".black-square");
     const buttons = document.querySelectorAll(".button");
 
     let currentInput = "";
     let isDatePart1Complete = false;
+
+
+    sidenavToggle.addEventListener("click", function() {
+        sidenav.classList.toggle("collapsed");
+
+        const header = document.querySelector("header");
+        if (sidenav.classList.contains("collapsed")) {
+            header.style.marginLeft = "0";  
+            header.style.width = "100%";  
+        } else {
+            header.style.marginLeft = "20%";  
+            header.style.width = "calc(100% - 20%)";  
+        }
+    });
+
 
     buttons.forEach(button => {
         button.addEventListener("click", function() {
