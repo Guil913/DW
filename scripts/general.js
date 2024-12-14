c40dedaeb76e919ea61112eccf5b0f8fd5cf82dc
Guil913 <guilhermecoelho913@gmail.com>
2024-12-14 03:37:11 +0000

    sidebar width toggle(needs on ,main)

--- scripts/general.js
+++ scripts/general.js
@@ -1,27 +1,19 @@
-function toggleSidenav() {
-    const sidenav = document.getElementById('sidenav');
-    const toggleButton = document.getElementById('sidenav-toggle');
-    const header = document.querySelector('header');
-    const mainContent = document.querySelector('.main-content');
+document.addEventListener("DOMContentLoaded", function() {
+    const sidenav = document.querySelector(".sidenav");
+    const sidenavToggle = document.querySelector(".sidenav-toggle");
 
 
-    const isCollapsed = sidenav.classList.toggle('collapsed');
+    sidenavToggle.addEventListener("click", function() {
+      
+        sidenav.classList.toggle("collapsed");
 
-  
-    toggleButton.innerHTML = isCollapsed ? '&#9654;' : '&#9664;';
-
-   
-    if (isCollapsed) {
-        header.style.marginLeft = '0';
-        header.style.width = '100%';
-    } else {
-        header.style.marginLeft = '20%';
-        header.style.width = 'calc(100% - 20%)';
-    }
-
-   
-    mainContent.style.marginLeft = isCollapsed ? '0' : '20%';
-}
-
-
-document.getElementById('sidenav-toggle').addEventListener('click', toggleSidenav);
+        const header = document.querySelector("header");
+        if (sidenav.classList.contains("collapsed")) {
+            header.style.marginLeft = "0";  
+            header.style.width = "100%";  
+        } else {
+            header.style.marginLeft = "20%";  
+            header.style.width = "calc(100% - 20%)";  
+        }
+    });
+});
