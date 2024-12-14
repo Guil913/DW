87a41d8d48cd7b1474db606eb51433aa7ce8368b
Guil913 <guilhermecoelho913@gmail.com>
2024-12-14 03:27:53 +0000

    header, more details

--- scripts/general.js
+++ scripts/general.js
@@ -1,14 +1,27 @@
 function toggleSidenav() {
-    const sidenav = document.querySelector('.sidenav');
+    const sidenav = document.getElementById('sidenav');
     const toggleButton = document.getElementById('sidenav-toggle');
-    const mainContent = document.querySelectorAll('.main, p');
+    const header = document.querySelector('header');
+    const mainContent = document.querySelector('.main-content');
+
 
     const isCollapsed = sidenav.classList.toggle('collapsed');
 
+  
     toggleButton.innerHTML = isCollapsed ? '&#9654;' : '&#9664;';
 
    
-    mainContent.forEach(element => {
-        element.style.marginLeft = isCollapsed ? '0' : '20%';
-    });
+    if (isCollapsed) {
+        header.style.marginLeft = '0';
+        header.style.width = '100%';
+    } else {
+        header.style.marginLeft = '20%';
+        header.style.width = 'calc(100% - 20%)';
+    }
+
+   
+    mainContent.style.marginLeft = isCollapsed ? '0' : '20%';
 }
+
+
+document.getElementById('sidenav-toggle').addEventListener('click', toggleSidenav);
